655c7fb555ce68c6c78d8bff42230da29ee1b3d2
Tanner Hamilton <155474958+TannerHamilton123@users.noreply.github.com>
2026-07-07 16:33:05 -0700

    Add new post entry for 'Boba Broker: My First Game Jam'

--- src/data/posts.js
+++ src/data/posts.js
@@ -4,6 +4,11 @@ const posts = [
     title: "Space: The Fiction's Frontier",
     date: "2026-01-29",
   },
+  {
+    slug: "my-first-game-jam",
+    title: "Boba Broker: My First Game Jam",
+    date: "2026-06-29",
+  },
 ];
 
 export default posts;
